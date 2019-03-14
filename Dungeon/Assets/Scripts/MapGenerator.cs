15e12219c468b1639408537f38c8fa3ffe7ba8b7
Nikita Kazantsev <kaznik2013@gmail.com>
2019-03-14 11:44:58 +0300

    new version

---
+++ Dungeon/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapGenerator : MonoBehaviour
+{
+    
+    public Tilemap tilemap;
+    public Tile floor;
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+            func();
+    }
+
+    void func(int[,] map)
+    {
+        
+        BoundsInt bounds = tilemap.cellBounds; // getting all allowed x and y in tilemap (getting its size)
+        TileBase[] allTiles = tilemap.GetTilesBlock(bounds); // getting info of tiles from tilemap
+        Vector3Int currentCell = tilemap.WorldToCell(transform.position); //current x,y,z position 
+
+        for (int x = 0; x < map.GetLength(0); ++x)
+        {
+            for (int y = 0; y < map.GetLength(1); ++y)
+            {
+                tilemap.SetTile(new Vector3Int(x, y, 0), floor);
+            }
+        }
+    }
+}
